a55fe3a3d388ca476d67494e2ed16131c07e5c43
Daanyaal Kapadia <daanyaalkapadia13@gmail.com>
2020-04-28 02:23:34 +0530

    First commit

---
+++ majorProject/app.js
@@ -0,0 +1,69 @@
+const   express         = require('express'),
+        mongoose        = require('mongoose'),
+        passport        = require('passport'),
+        flash           = require('connect-flash'),
+        session         = require('express-session'),
+        expressLayouts  = require('express-ejs-layouts');
+
+const app   = express();
+
+//Firebase Setup
+var firebase = require('firebase');
+var firebaseConfig = {
+    apiKey: "AIzaSyA4efHd6EbCDNSsAnNtT8kzdSXcHhbvfto",
+    authDomain: "all-india-grievance-app.firebaseapp.com",
+    databaseURL: "https://all-india-grievance-app.firebaseio.com",
+    projectId: "all-india-grievance-app",
+    storageBucket: "all-india-grievance-app.appspot.com",
+    messagingSenderId: "44004433146",
+    appId: "1:44004433146:web:56857ca37a36f351163d43",
+    measurementId: "G-B256703JWQ"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+app.use(express.static("public"));
+
+//Passport Config
+require('./config/passport')(passport);
+
+// DB config
+const db = require('./config/keys').MongoURI;
+
+//Connect to MongoDB
+mongoose.connect(db,{useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+// Bodyparser
+app.use(express.urlencoded({ extended:false }));    
+
+//Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+}));
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine','ejs');
+
+//Router
+app.use('/',require('./routes/index'));
+app.use('/',require('./routes/forgot'));
+app.use('/users',require('./routes/users'));
+
+// Common Route
+app.get('*',function(req,res){
+    res.render('login');
+});
+
+const PORT = process.env.PORT || 80
+
+app.listen(PORT, console.log('Server Started!!!!'));
---
+++ majorProject/routes/index.js
@@ -0,0 +1,127 @@
+const   express                 = require('express'),
+        router                  = express.Router(),
+        firebase                = require('firebase'),
+        { ensureAuthenticated } = require('../config/auth');
+
+var datab = firebase.database();
+var ref = datab.ref("/user");
+
+const User = require('../models/User')
+
+router.get('/', (req,res) => res.redirect('/users/login'));
+
+var totalcount=0;
+var totalredressed=0;
+var totalpending=0;
+var totalusers=0;
+var userArray=[];
+var userSubArray=[];
+
+//Dashboard Route
+router.get('/dashboard',ensureAuthenticated,function(req,res){
+    ref.once("value", function(snapshot) {
+        var allUsers = snapshot.val();   //Data is in JSON format.
+        if(req.user.state == "Main"){
+            Object.keys(allUsers).forEach((user) => {
+                totalusers=totalusers+1;
+                userSubArray.push(allUsers[user].fname);
+                userSubArray.push(allUsers[user].state);
+                userSubArray.push(allUsers[user].mobile);
+                userSubArray.push(allUsers[user].email);
+                userArray.push(userSubArray);
+                userSubArray=[];
+                if(allUsers[user].complaints)
+                {
+                    Object.keys(allUsers[user].complaints).forEach((complaint) => {
+                        totalcount=totalcount+1;
+                        if(allUsers[user].complaints[complaint].status == "Complaint Solved"){
+                            totalredressed=totalredressed+1;
+                        }
+                        if(allUsers[user].complaints[complaint].status == "Complaint Not Solved"){
+                            totalpending=totalpending+1;
+                        }
+                    });
+                }
+            });
+        }else{
+            Object.keys(allUsers).forEach((user) => {
+                if(allUsers[user].state == req.user.state){
+                    totalusers=totalusers+1;
+                    userSubArray.push(allUsers[user].fname);
+                    userSubArray.push(allUsers[user].city);
+                    userSubArray.push(allUsers[user].mobile);
+                    userSubArray.push(allUsers[user].email);
+                    userArray.push(userSubArray);
+                    userSubArray=[];
+                }
+                if(allUsers[user].complaints)
+                {
+                    Object.keys(allUsers[user].complaints).forEach((complaint) => {
+                        if(allUsers[user].complaints[complaint].state == req.user.state)
+                        {
+                            totalcount=totalcount+1;
+                            if(allUsers[user].complaints[complaint].status == "Complaint Solved"){
+                                totalredressed=totalredressed+1;
+                            }
+                            if(allUsers[user].complaints[complaint].status == "Complaint Not Solved"){
+                                totalpending=totalpending+1;
+                            }
+                        }
+                    });
+                }
+            });
+        }
+        User.find().exec().then(function(numItems) {
+            res.render("dashboard.ejs",{name: req.user.name, state: req.user.state,
+                totalcount: totalcount, totalredressed:totalredressed, totalpending:totalpending ,totalusers:totalusers,
+                tableData:numItems, userArray:userArray});
+                totalredressed=0;
+                totalusers=0;
+                totalcount=0;
+                totalpending=0;
+                userArray=[];
+        }); 
+    });
+});
+
+//To display Admin list in new page
+router.get('/adminlist',ensureAuthenticated,function(req,res){
+    User.find().exec().then(function(numItems) {
+        res.render("adminlist.ejs",{name: req.user.name, state: req.user.state,tableData : numItems});
+    });
+});
+
+//UserList
+router.get('/userlist',ensureAuthenticated,function(req,res){
+    ref.once("value", function(snapshot) {
+        var allUsers = snapshot.val();   //Data is in JSON format.
+        if(req.user.state == "Main"){
+            Object.keys(allUsers).forEach((user) => {
+                totalusers=totalusers+1;
+                userSubArray.push(allUsers[user].fname);
+                userSubArray.push(allUsers[user].state);
+                userSubArray.push(allUsers[user].mobile);
+                userSubArray.push(allUsers[user].email);
+                userArray.push(userSubArray);
+                userSubArray=[];
+                
+            });
+        }else{
+            Object.keys(allUsers).forEach((user) => {
+                if(allUsers[user].state == req.user.state){
+                    totalusers=totalusers+1;
+                    userSubArray.push(allUsers[user].fname);
+                    userSubArray.push(allUsers[user].city);
+                    userSubArray.push(allUsers[user].mobile);
+                    userSubArray.push(allUsers[user].email);
+                    userArray.push(userSubArray);
+                    userSubArray=[];
+                }
+            });
+        }
+        res.render("userlist.ejs",{name: req.user.name, state: req.user.state, userArray:userArray});
+        userArray=[]; 
+    });
+});
+
+module.exports = router;
